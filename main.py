40d36b403723a70b97a4c26fa556f7beca7379b3
shiv <sudalasr@mail.uc.edu>
2020-11-26 18:46:52 -0500

    added bgd, sgd, decaying rates, adaptive rates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ __license__ = "MIT"
 '''
 Perceptron classifier
 '''
-from test import *
+from perceptron import *
 np.random.seed(0)
 
 
@@ -20,7 +20,16 @@ def gen_syn_data(num_points, num_features, test_size):
 
 
 X_train, X_test, y_train, y_test = gen_syn_data(100, 2, 0.5)
+
+# fig, axs = plt.subplots(4)
+# for i, lr in enumerate([0.1, 0.01, 0.001, 0.0001]):
+#     model = Perceptron()
+#     model.fit(X_train, y_train, 100, 0.001, optimizer='stochastic')
+#     axs[i].plot(model.errors)
+# plt.show()
+
+
 model = Perceptron()
-model.fit(X_train, y_train, 100, 0.001, optimizer='batch', plot_decision=True)
-y_preds = model.predict(X_test)
-print(classification_report(y_test, y_preds))
+model.fit(X_train, y_train, 100, 0.001, optimizer='batch', adaptive_rate_threshold=1.02, early_stopping=True)
+plt.plot(model.errors)
+plt.show()
--- perceptron.py
+++ perceptron.py
@@ -8,6 +8,7 @@ class Perceptron:
     def __init__(self):
         self.w = None
         self.b = None
+        self.errors = []
 
     def threshold_function(self, x):
         fx = np.dot(self.w, x)+self.b
@@ -20,16 +21,17 @@ class Perceptron:
 
         return y_pred
 
-    def fit(self, X, y, epochs=50, alpha=0.1, optimizer='stochastic', return_error_plot=False, plot_decision=True):
+    def fit(self, X, y, epochs=50, alpha=0.1, optimizer='stochastic', plot_error=False, plot_decision=False,
+            decaying_rate=None, adaptive_rate_threshold=None, early_stopping=False):
         self.w = np.random.randn(X.shape[1]).reshape(1, X.shape[1])
         self.b = 0
         weights_history = []
         bias_history = []
-        errors = []
 
         for i in range(epochs):
             sum_dw = np.zeros(X.shape[1]).reshape(1, X.shape[1])
             sum_db = 0
+
             for xi, yi in zip(X, y):
                 y_pred = self.threshold_function(xi)
                 dw = alpha * (yi - y_pred) * xi
@@ -49,14 +51,35 @@ class Perceptron:
                 bias_history.append(self.b)
 
             error = mean_squared_error(y, self.predict(X))
-            errors.append(error)
+            # print(error)
+            self.errors.append(error)
+
+            if early_stopping:
+                if error <= 0.01:
+                    print('Stopping Training')
+                    break
+
+            if decaying_rate:
+                alpha = alpha * decaying_rate
+                # print('a: ', alpha)
+
+            if adaptive_rate_threshold:
+                if i > 0:
+                    if self.errors[-1] - self.errors[-2] > adaptive_rate_threshold:
+                        self.w = weights_history[-2]
+                        self.b = bias_history[-2]
+                        alpha = alpha * 0.9
+                        # print('a: ', alpha)
+                    else:
+                        alpha = alpha * 1.1
+                        # print('a: ', alpha)
 
             if plot_decision:
                 if i+1 in [5, 10, 50, 100]:
                     self.decision_boundary(X, y, title='{} epoch: {}'.format(optimizer, i+1))
 
-        if return_error_plot:
-            plt.plot(errors)
+        if plot_error:
+            plt.plot(self.errors)
             plt.show()
 
     def decision_boundary(self, X, y, title):
@@ -85,6 +108,6 @@ class Perceptron:
         plt.title('Perceptron Decision Boundary - {}'.format(title))
         plt.xlabel('x1')
         plt.ylabel('x2')
-        plt.savefig('figs/{}.pdf'.format(title))
+        # plt.savefig('figs/{}.pdf'.format(title))
         plt.show()
         plt.clf()
